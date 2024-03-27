943637675cf113442c058a9cd217786c8e63ff36
test <ten13zin@gmail.com>
2024-03-27 11:07:41 +0530

    read page

--- main.py
+++ main.py
@@ -17,10 +17,23 @@ def get_text_list(pecha: str):
     return json.load(toc_fn.open("r"))
 
 
-@app.get("/{pecha}/texts/{text_id}/pages")
+@app.get("/{pecha}/{text_id}/pages")
 def get_page_list(pecha: str, text_id: str):
     pecha_path = config.DATA_PATH / pecha
     text_path = pecha_path / text_id
     if not text_path.exists():
         return HTTPException(status_code=404, detail="text not found")
     return list(set([fn.stem for fn in text_path.iterdir()]))
+
+
+@app.get("/{pecha}/{text_id}/{page_id}")
+def read_page(pecha: str, text_id: str, page_id: str):
+    pecha_path = config.DATA_PATH / pecha
+    text_path = pecha_path / text_id
+    page_content_fn = text_path / f"{page_id}.txt"
+    page_img_fn = text_path / f"{page_id}.img"
+
+    return {
+        "image_url": page_img_fn.read_text().strip(),
+        "content": page_content_fn.read_text().strip(),
+    }
