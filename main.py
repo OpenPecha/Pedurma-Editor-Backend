3513396016af2a2c4aaa8802e5e79493608ae7ca
test <ten13zin@gmail.com>
2024-03-27 11:00:59 +0530

    get page list

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ app = FastAPI()
 
 
 @app.get("/{pecha}/texts")
-def read_item(pecha: str):
+def get_text_list(pecha: str):
     pecha_path = config.DATA_PATH / pecha
-    if not pecha_path.exists():
+    toc_fn = pecha_path / "toc.json"
+    if not pecha_path.exists() or not toc_fn.exists():
         return HTTPException(status_code=404, detail="pecha not found")
     toc_fn = pecha_path / "toc.json"
     return json.load(toc_fn.open("r"))
+
+
+@app.get("/{pecha}/texts/{text_id}/pages")
+def get_page_list(pecha: str, text_id: str):
+    pecha_path = config.DATA_PATH / pecha
+    text_path = pecha_path / text_id
+    if not text_path.exists():
+        return HTTPException(status_code=404, detail="text not found")
+    return list(set([fn.stem for fn in text_path.iterdir()]))
