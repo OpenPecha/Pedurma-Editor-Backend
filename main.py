e96cda9445baf98f0b8874f978818c8337f07a5f
test <ten13zin@gmail.com>
2024-03-27 12:27:24 +0530

    setup CROS

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import json
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, HttpUrl
 
 import config
 
 app = FastAPI()
+
+origins = ["http://localhost:9000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Text(BaseModel):
